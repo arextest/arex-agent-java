1a1d16545523c3e9c667b770c724af41571ff3c0
mr3 <mr3.sole@gmail.com>
2022-06-06 15:25:05 +0800

    feat: support lettuce (#11)

---
+++ arex-instrumentation/redis/arex-lettuce-v6/src/main/java/io/arex/inst/lettuce/v6/StatefulRedisConnectionImplInstrumentation.java
@@ -0,0 +1,71 @@
+package io.arex.inst.lettuce.v6;
+
+import io.arex.foundation.api.MethodInstrumentation;
+import io.arex.foundation.api.TypeInstrumentation;
+import io.lettuce.core.RedisAsyncCommandsImpl;
+import io.lettuce.core.RedisReactiveCommandsImpl;
+import io.lettuce.core.StatefulRedisConnectionImpl;
+import io.lettuce.core.codec.RedisCodec;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static net.bytebuddy.matcher.ElementMatchers.isProtected;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+/**
+ * StatefulRedisConnectionImplInstrumentation
+ */
+public class StatefulRedisConnectionImplInstrumentation extends TypeInstrumentation {
+    @Override
+    protected ElementMatcher<TypeDescription> typeMatcher() {
+        return named("io.lettuce.core.StatefulRedisConnectionImpl");
+    }
+
+    @Override
+    public List<MethodInstrumentation> methodAdvices() {
+        ElementMatcher<MethodDescription> asyncMatcher = isProtected().and(named("newRedisAsyncCommandsImpl"));
+
+        String asyncAdvice = this.getClass().getName() + "$NewRedisAsyncCommandsImplAdvice";
+
+        ElementMatcher<MethodDescription> reactiveMatcher = isProtected().and(named("newRedisReactiveCommandsImpl"));
+
+        String reactiveAdvice = this.getClass().getName() + "$NewRedisReactiveCommandsImplAdvice";
+
+        return Arrays.asList(new MethodInstrumentation(asyncMatcher, asyncAdvice),
+            new MethodInstrumentation(reactiveMatcher, reactiveAdvice));
+    }
+
+    public static class NewRedisAsyncCommandsImplAdvice {
+        @Advice.OnMethodEnter(skipOn = Advice.OnDefaultValue.class)
+        public static boolean onEnter() {
+            return false;
+        }
+
+        @Advice.OnMethodExit
+        public static <K, V> void onExit(@Advice.This StatefulRedisConnectionImpl<K, V> connection,
+            @Advice.FieldValue("codec") RedisCodec<K, V> codec,
+            @Advice.Return(readOnly = false) RedisAsyncCommandsImpl<K, V> returnValue) {
+            returnValue = new RedisAsyncCommandsImplWrapper<>(connection, codec);
+        }
+    }
+
+    public static class NewRedisReactiveCommandsImplAdvice {
+        @Advice.OnMethodEnter(skipOn = Advice.OnDefaultValue.class)
+        public static boolean onEnter() {
+            return false;
+        }
+
+        @Advice.OnMethodExit
+        public static <K, V> void onExit(@Advice.This StatefulRedisConnectionImpl<K, V> connection,
+            @Advice.FieldValue("codec") RedisCodec<K, V> codec,
+            @Advice.Return(readOnly = false) RedisReactiveCommandsImpl<K, V> returnValue) {
+            returnValue = new RedisReactiveCommandsImplWrapper<>(connection, codec);
+        }
+    }
+}
---
+++ arex-instrumentation/redis/arex-redis-common/src/main/java/io/arex/inst/jedis/common/RedisExtractor.java
@@ -0,0 +1,45 @@
+package io.arex.inst.jedis.common;
+
+import io.arex.foundation.model.JedisMocker;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RedisExtractor {
+    private final String clusterName;
+    private final String command;
+    private final String key;
+    private final String field;
+
+    public RedisExtractor(String url, String method, String key, String field) {
+        this.clusterName = RedisCluster.get(url);
+        this.command = method;
+        this.key = key;
+        this.field = field;
+    }
+
+    public void record(Object response) {
+        JedisMocker mocker = new JedisMocker(this.clusterName, this.key, this.command, this.field, response);
+        mocker.record();
+    }
+
+    public void record(Throwable exception) {
+        JedisMocker mocker = new JedisMocker(this.clusterName, this.key, this.command, this.field, null);
+        mocker.setExceptionMessage(exception.getMessage());
+        mocker.record();
+    }
+
+    public Object replay() {
+        JedisMocker mocker = new JedisMocker(this.clusterName, this.key, this.command, this.field);
+        return mocker.replay();
+    }
+
+    static class RedisCluster {
+        private final static ConcurrentHashMap<String, String> REDIS_CLUSTER_CACHE = new ConcurrentHashMap<>(5);
+        private final static AtomicInteger sequence = new AtomicInteger();
+
+        static String get(String key) {
+            return REDIS_CLUSTER_CACHE.computeIfAbsent(key, k -> "Cluster" + sequence.addAndGet(1));
+        }
+    }
+}
