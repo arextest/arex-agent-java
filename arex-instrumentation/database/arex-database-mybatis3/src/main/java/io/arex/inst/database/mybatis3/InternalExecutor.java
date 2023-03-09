c2591dd7b5d19fc281412fa62f8b86317e9a8b03
YongwuHe <840985000@qq.com>
2023-03-09 16:36:01 +0800

    feat: support mybatis batch insert

--- arex-instrumentation/database/arex-database-mybatis3/src/main/java/io/arex/inst/database/mybatis3/InternalExecutor.java
+++ arex-instrumentation/database/arex-database-mybatis3/src/main/java/io/arex/inst/database/mybatis3/InternalExecutor.java
@@ -4,11 +4,11 @@ import io.arex.agent.bootstrap.model.MockResult;
 import io.arex.agent.bootstrap.util.StringUtil;
 import io.arex.inst.runtime.serializer.Serializer;
 import io.arex.inst.database.common.DatabaseExtractor;
+
+import org.apache.ibatis.binding.MapperMethod.ParamMap;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.reflection.Reflector;
-
-import java.util.Map;
 
 public class InternalExecutor {
 
@@ -17,11 +17,7 @@ public class InternalExecutor {
 
     public static MockResult replay(MappedStatement ms, Object o, BoundSql boundSql, String methodName) {
         DatabaseExtractor extractor = createExtractor(ms, boundSql, o, methodName);
-        MockResult replayResult = extractor.replay();
-        if (containKeyHolder(ms, extractor, o)) {
-            restoreKeyHolder(ms, extractor, o);
-        }
-        return replayResult;
+        return extractor.replay();
     }
 
     public static MockResult replay(DatabaseExtractor extractor, MappedStatement ms, Object o) {
@@ -34,10 +30,6 @@ public class InternalExecutor {
 
     public static <U> void record(MappedStatement ms, Object o, BoundSql boundSql, U result, Throwable throwable, String methodName) {
         DatabaseExtractor extractor = createExtractor(ms, boundSql, o, methodName);
-        if (containKeyHolder(ms, extractor, o)) {
-            saveKeyHolder(ms, extractor, o);
-        }
-
         if (throwable != null) {
             extractor.record(throwable);
         } else {
@@ -60,7 +52,8 @@ public class InternalExecutor {
 
     private static void restoreKeyHolder(MappedStatement ms, DatabaseExtractor executor, Object o) {
         String[] keyHolderList = StringUtil.split(executor.getKeyHolder(), KEYHOLDER_SEPARATOR);
-        String[] keyProperties = ms.getKeyProperties();
+        Object insertEntity = o instanceof ParamMap ? getEntityFromMap((ParamMap<?>) o) : o;
+        String[] keyProperties = o instanceof ParamMap ? transformerProperties(ms.getKeyProperties()) : ms.getKeyProperties();
 
         if (keyHolderList == null || keyProperties == null) {
             return;
@@ -70,30 +63,61 @@ public class InternalExecutor {
             return;
         }
 
+        if (insertEntity == null) {
+            return;
+        }
+
         try {
-            Reflector reflector = new Reflector(o.getClass());
+            Reflector reflector = new Reflector(insertEntity.getClass());
             for (int i = 0; i < keyHolderList.length; i++) {
                 String[] valueType = StringUtil.split(keyHolderList[i], KEYHOLDER_TYPE_SEPARATOR);
                 Object keyHolderValue = Serializer.deserialize(valueType[0], valueType[1]);
-                reflector.getSetInvoker(keyProperties[i]).invoke(o, new Object[]{keyHolderValue});
+                reflector.getSetInvoker(keyProperties[i]).invoke(insertEntity, new Object[]{keyHolderValue});
             }
         } catch (Exception ignored) {}
     }
 
     private static void saveKeyHolder(MappedStatement ms, DatabaseExtractor executor, Object o) {
         StringBuilder builder = new StringBuilder();
-        Reflector reflector = new Reflector(o.getClass());
-        for (String keyHolderName : ms.getKeyProperties()) {
+
+        Object insertEntity = o instanceof ParamMap ? getEntityFromMap((ParamMap<?>) o) : o;
+        String[] primaryKeyNames = o instanceof ParamMap ? transformerProperties(ms.getKeyProperties()) : ms.getKeyProperties();
+
+        if (insertEntity == null) {
+            return;
+        }
+
+        Reflector reflector = new Reflector(insertEntity.getClass());
+
+        for (int i = 0; i < primaryKeyNames.length; i++) {
             try {
-                Object keyHolderValue = reflector.getGetInvoker(keyHolderName).invoke(o, null);
+                Object keyHolderValue = reflector.getGetInvoker(primaryKeyNames[i]).invoke(insertEntity, null);
                 if (keyHolderValue == null) {
                     continue;
                 }
-                builder.append(keyHolderValue).append(KEYHOLDER_TYPE_SEPARATOR).append(keyHolderValue.getClass().getName()).append(KEYHOLDER_SEPARATOR);
+                builder.append(keyHolderValue).append(KEYHOLDER_TYPE_SEPARATOR).append(keyHolderValue.getClass().getName());
+                if (i < primaryKeyNames.length - 1) {
+                    builder.append(KEYHOLDER_SEPARATOR);
+                }
             } catch (Exception ignored) {
             }
         }
         executor.setKeyHolder(builder.toString());
+    }
+
+    private static String[] transformerProperties(String[] keyProperties) {
+        String[] primaryKeyNames = new String[keyProperties.length];
+        for (int i = 0; i < keyProperties.length; i++) {
+            int firstDot = keyProperties[i].indexOf(".");
+            if (firstDot != -1) {
+                primaryKeyNames[i] = StringUtil.substring(keyProperties[i], firstDot + 1);
+            }
+        }
+        return primaryKeyNames;
+    }
+
+    private static Object getEntityFromMap(ParamMap<?> paramMap) {
+        return paramMap.values().stream().findFirst().orElse(null);
     }
 
     private static boolean containKeyHolder(MappedStatement ms, DatabaseExtractor executor, Object o) {
@@ -101,7 +125,11 @@ public class InternalExecutor {
             return false;
         }
 
-        return StringUtil.containsIgnoreCase(executor.getSql(), "insert") && !(o instanceof Map);
+        if (o instanceof ParamMap && ((ParamMap<?>) o).size() == 0) {
+            return false;
+        }
+
+        return StringUtil.containsIgnoreCase(executor.getSql(), "insert");
     }
 
     public static DatabaseExtractor createExtractor(MappedStatement mappedStatement,
