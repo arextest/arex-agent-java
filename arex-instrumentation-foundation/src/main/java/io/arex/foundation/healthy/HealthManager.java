a48e7545aa4e2942d7867789459306cef72b774e
Lucas <vipmyx8651@163.com>
2023-09-04 19:34:06 +0800

    feat: optimize report rate (#276)
    * feat: optimize record rate and decelerate
    
    * feat: add UT

--- arex-instrumentation-foundation/src/main/java/io/arex/foundation/healthy/HealthManager.java
+++ arex-instrumentation-foundation/src/main/java/io/arex/foundation/healthy/HealthManager.java
@@ -11,7 +11,6 @@ import io.arex.inst.runtime.log.LogManager;
 import com.google.common.util.concurrent.RateLimiter;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -21,6 +20,21 @@ import static io.arex.foundation.healthy.HealthManager.RecordRateManager.RECORD_
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+/**
+ * health check rule: <pre>
+ * 1. queue overflow:
+ * switch to reject state(not record), decrease record rate 20% and reopen record after 30 seconds,
+ * then check state after 5 minutes(statistic data during this period),
+ * if not recover, continue decrease record rate 20%, check state again after 10 minutes.
+ * if recover, switch to normal state, end check and record rate will be recovered in next request(acquire)
+ *
+ * 2. storage service exception:
+ * switch to reject state(not record), reopen record after 10 seconds and check state after 3 minutes
+ * statistic data during this period,
+ * if not recover, decrease to minimum record rate(about 3 case in 100 min), check state again after 60 minutes.
+ * if recover, switch to normal state, end check and record rate will be recovered in next request(acquire)
+ * </pre>
+ */
 public class HealthManager {
 
     static final int NORMAL = 0;
@@ -57,6 +71,7 @@ public class HealthManager {
 
     public static void onEnqueueRejection() {
         if (STATE.compareAndSet(NORMAL, FAST_REJECT)) {
+            LogManager.warn("healthManager.enqueueRejection", "queue overflow! switch to reject state");
             // Check state after 30 seconds
             scheduledFuture = TimerService.schedule(() -> {
                 RECORD_RATE_MANAGER.decelerate();
@@ -68,7 +83,7 @@ public class HealthManager {
 
     public static void onDataServiceRejection() {
         if (STATE.compareAndSet(NORMAL, FAST_REJECT) || STATE.compareAndSet(QUEUE_OVERFLOW, FAST_REJECT)) {
-            LogManager.warn("onDataServiceRejection", "data service error! switch to reject state");
+            LogManager.warn("healthManager.dataServiceRejection", "data service error! switch to reject state");
             if (scheduledFuture != null) {
                 scheduledFuture.cancel(false);
             }
@@ -110,11 +125,10 @@ public class HealthManager {
                 case SERVICE_EXCEPTION:
                     isRecover = DATA_SERVICE_CHECKER.isRecover();
                     if (isRecover) {
-                        RECORD_RATE_MANAGER.changeRate(false);
                         STATE.set(NORMAL);
                         scheduledFuture = null;
                     } else {
-                        RECORD_RATE_MANAGER.changeRate(true);
+                        RECORD_RATE_MANAGER.changeRate();
                         scheduledFuture = TimerService.schedule(this, 60, MINUTES);
                     }
                     break;
@@ -131,7 +145,8 @@ public class HealthManager {
                 default:
                     break;
             }
-            LogManager.warn("healthCheckTask", StringUtil.format("check result: state=%s, isRecover=%s", String.valueOf(STATE.get()), String.valueOf(isRecover)));
+            LogManager.warn("healthManager.healthCheckTask", StringUtil.format("check result: state=%s, isRecover=%s",
+                    String.valueOf(STATE.get()), String.valueOf(isRecover)));
         }
     }
 
@@ -163,8 +178,8 @@ public class HealthManager {
             }
 
             boolean isRecover = (fatal.get() < 3 && ((double)success.get() / total.get()) >= 0.99);
-            LogManager.warn("isRecover", StringUtil.format("fatal=%s, success=%s, total=%s",
-                    String.valueOf(fatal), String.valueOf(success), String.valueOf(total)));
+            LogManager.warn("healthManager.isRecover", StringUtil.format("isRecover=%s, fatal=%s, success=%s, total=%s",
+                    String.valueOf(isRecover), String.valueOf(fatal), String.valueOf(success), String.valueOf(total)));
             reset();
             return isRecover;
         }
@@ -183,44 +198,74 @@ public class HealthManager {
         static final double MIN_RATE = 0.03D;
         // send a permit in 60 seconds at least
         static final long BASE = MINUTES.toSeconds(1);
-
         /**
-         * Record rate acquire
+         * Record rate acquires
          */
-        boolean acquire(String methodName, double rate) {
-            if (rate <= 0) {
+        boolean acquire(String methodName, double configRate) {
+            if (configRate <= 0) {
                 return false;
             }
             Pair<Double, RateLimiter> rateLimiterPair = RATE_LIMITER_MAP.computeIfAbsent(methodName, key -> {
-                return Pair.of(rate, RateLimiter.create(rate / BASE));
+                double targetRate = getCurrentRate(configRate);
+                return Pair.of(targetRate, RateLimiter.create(targetRate / BASE));
             });
 
-            int cmp = Double.compare(Optional.ofNullable(rateLimiterPair.getFirst()).orElse(0d), rate);
-            if ((cmp < 0 && STATE.get() == NORMAL) || cmp == 1) {
-                rateLimiterPair = RATE_LIMITER_MAP.put(methodName, Pair.of(rate, RateLimiter.create(rate / BASE)));
-            }
-
+            updateRate(configRate, rateLimiterPair.getFirst());
+            rateLimiterPair = RATE_LIMITER_MAP.get(methodName);
             return rateLimiterPair != null && rateLimiterPair.getSecond().tryAcquire();
+        }
+
+        /**
+         * custom modify config rate or health check recover :
+         * 1. increase or recover (configRate > currentRate && STATE == NORMAL)
+         * 2. decrease (configRate < currentRate)
+         */
+        void updateRate(double configRate, double currentRate) {
+            if ((configRate > currentRate && STATE.get() == NORMAL) || configRate < currentRate) {
+                for (Map.Entry<String, Pair<Double, RateLimiter>> entry : RATE_LIMITER_MAP.entrySet()) {
+                    RATE_LIMITER_MAP.put(entry.getKey(), Pair.of(configRate, RateLimiter.create(configRate / BASE)));
+                }
+                System.setProperty(ConfigConstants.CURRENT_RATE, String.format("%.2f", configRate));
+                System.setProperty(ConfigConstants.DECELERATE_CODE, DecelerateReasonEnum.NORMAL.getCodeStr());
+                LogManager.info("healthManager.updateRate",
+                        StringUtil.format("update rate, current rate change to: %s", String.format("%.2f", configRate)));
+            }
+        }
+
+        double getCurrentRate(double rate) {
+            if (STATE.get() == NORMAL) {
+                return rate;
+            }
+            // un normally state, use current rate(maybe decelerated)
+            String currentRate = System.getProperty(ConfigConstants.CURRENT_RATE);
+            if (StringUtil.isNotEmpty(currentRate)) {
+                return Double.parseDouble(currentRate);
+            }
+            return rate;
         }
 
         boolean validate() {
             return ConfigManager.INSTANCE.getRecordRate() > 0;
         }
 
-        void changeRate(boolean useMinRate) {
+        /**
+         * Decrement record rate to {@link #MIN_RATE}
+         * trigger if service exception
+         */
+        void changeRate() {
             if (!validate()) {
                 return;
             }
             double targetRate = ConfigManager.INSTANCE.getRecordRate();
             for (Map.Entry<String, Pair<Double, RateLimiter>> entry : RATE_LIMITER_MAP.entrySet()) {
-                Pair<Double, RateLimiter> limiterPair = entry.getValue();
-                targetRate = useMinRate ? MIN_RATE : limiterPair.getFirst();
-                RATE_LIMITER_MAP.put(entry.getKey(), Pair.of(limiterPair.getFirst(), RateLimiter.create(targetRate / BASE)));
+                targetRate = MIN_RATE;
+                RATE_LIMITER_MAP.put(entry.getKey(), Pair.of(targetRate, RateLimiter.create(targetRate / BASE)));
             }
-            System.setProperty(ConfigConstants.CURRENT_RATE, String.valueOf(targetRate));
-            // if useMinRate is false, means recover from service exception
-            String decelerateCode = useMinRate ? DecelerateReasonEnum.SERVICE_EXCEPTION.getCodeStr() : DecelerateReasonEnum.NORMAL.getCodeStr();
-            System.setProperty(ConfigConstants.DECELERATE_CODE, decelerateCode);
+            System.setProperty(ConfigConstants.CURRENT_RATE, String.format("%.2f", targetRate));
+            System.setProperty(ConfigConstants.DECELERATE_CODE, DecelerateReasonEnum.SERVICE_EXCEPTION.getCodeStr());
+            LogManager.warn("healthManager.decelerate",
+                    StringUtil.format("service exception! decrement record rate, current rate change to: %s",
+                            String.format("%.2f", targetRate)));
         }
 
         /**
@@ -239,32 +284,10 @@ public class HealthManager {
             }
             System.setProperty(ConfigConstants.CURRENT_RATE, String.format("%.2f", targetRate));
             System.setProperty(ConfigConstants.DECELERATE_CODE, DecelerateReasonEnum.QUEUE_OVERFLOW.getCodeStr());
-            LogManager.warn("onEnqueueRejection.decelerate",
+            LogManager.warn("healthManager.decelerate",
                     StringUtil.format("queue overflow! decrement record rate, current rate change to: %s",
                             String.format("%.2f", targetRate)));
         }
 
-        /**
-         * Increment record rate to config
-         * trigger if queue is empty
-         */
-        void accelerate() {
-            if (!validate()) {
-                return;
-            }
-
-            for (Map.Entry<String, Pair<Double, RateLimiter>> entry : RATE_LIMITER_MAP.entrySet()) {
-                Pair<Double, RateLimiter> limiterPair = entry.getValue();
-                Double currentRate = limiterPair.getFirst();
-                double targetRate = Math.min(currentRate * 1.2, limiterPair.getSecond().getRate() * BASE);
-                int cmp = Double.compare(currentRate, targetRate);
-                if (cmp < 0) {
-                    RATE_LIMITER_MAP.put(entry.getKey(), Pair.of(limiterPair.getFirst(), RateLimiter.create(targetRate / BASE)));
-                } else if (cmp == 0) {
-                    STATE.set(NORMAL);
-                    break;
-                }
-            }
-        }
     }
 }
--- arex-instrumentation-foundation/src/test/java/io/arex/foundation/healthy/HealthManagerTest.java
+++ arex-instrumentation-foundation/src/test/java/io/arex/foundation/healthy/HealthManagerTest.java
@@ -1,18 +1,53 @@
 package io.arex.foundation.healthy;
 
 import io.arex.agent.bootstrap.constants.ConfigConstants;
+import io.arex.agent.bootstrap.util.Assert;
+import io.arex.agent.bootstrap.util.ReflectUtil;
 import io.arex.foundation.config.ConfigManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import io.arex.foundation.services.TimerService;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.atLeastOnce;
 
 class HealthManagerTest {
 
+    static AtomicInteger STATE = new AtomicInteger(HealthManager.NORMAL);
+    static MockedStatic<TimerService> timerServiceMocker;
+    static HealthManager.HealthCheckTask healthCheckTask;
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        Object stateObj = ReflectUtil.getFieldOrInvokeMethod(() -> HealthManager.class.getDeclaredField("STATE"),null);
+        if (stateObj instanceof AtomicInteger) {
+            STATE = (AtomicInteger) stateObj;
+        }
+        timerServiceMocker = Mockito.mockStatic(TimerService.class);
+        healthCheckTask = new HealthManager.HealthCheckTask();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        timerServiceMocker = null;
+        healthCheckTask = null;
+        Mockito.clearAllCaches();
+    }
+
     @BeforeEach
-    void setUp() {
+    void before() {
+        STATE.set(HealthManager.NORMAL);
         System.clearProperty(ConfigConstants.CURRENT_RATE);
     }
 
@@ -31,11 +66,11 @@ class HealthManagerTest {
     @Test
     void changeRate() {
         ConfigManager.INSTANCE.setRecordRate(0);
-        HealthManager.RecordRateManager.RECORD_RATE_MANAGER.changeRate(false);
+        HealthManager.RecordRateManager.RECORD_RATE_MANAGER.changeRate();
         assertNull(System.getProperty(ConfigConstants.CURRENT_RATE));
         ConfigManager.INSTANCE.setRecordRate(1);
         HealthManager.acquire("mock");
-        HealthManager.RecordRateManager.RECORD_RATE_MANAGER.changeRate(true);
+        HealthManager.RecordRateManager.RECORD_RATE_MANAGER.changeRate();
         assertEquals(String.valueOf(HealthManager.RecordRateManager.MIN_RATE),
                 System.getProperty(ConfigConstants.CURRENT_RATE));
     }
@@ -49,5 +84,67 @@ class HealthManagerTest {
         HealthManager.acquire("mock");
         HealthManager.RecordRateManager.RECORD_RATE_MANAGER.decelerate();
         assertEquals("0.80", System.getProperty(ConfigConstants.CURRENT_RATE));
+    }
+
+    @Test
+    void onEnqueueRejection() {
+        HealthManager.onEnqueueRejection();
+        timerServiceMocker.verify(() -> TimerService.schedule(any(), anyLong(), any()));
+    }
+
+    @Test
+    void onDataServiceRejection() {
+        HealthManager.onDataServiceRejection();
+        timerServiceMocker.verify(() -> TimerService.schedule(any(), anyLong(), any()), atLeastOnce());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 0, 0",
+            "2, 1, 1"
+    })
+    void getCurrentRate(int state, double rate, double expect) {
+        STATE.set(state);
+        System.setProperty(ConfigConstants.CURRENT_RATE, rate == 0 ? "" : String.valueOf(rate));
+        double result = HealthManager.RecordRateManager.RECORD_RATE_MANAGER.getCurrentRate(rate);
+        assertEquals(expect, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("runCase")
+    void run(Runnable mocker, Assert asserts) {
+        mocker.run();
+        healthCheckTask.run();
+        asserts.verity();
+    }
+
+    static Stream<Arguments> runCase() {
+        Runnable serviceExceptionMocker1 = () -> {
+            STATE.set(HealthManager.SERVICE_EXCEPTION);
+        };
+        Runnable serviceExceptionMocker2 = () -> {
+            serviceExceptionMocker1.run();
+            HealthManager.reportUsedTime(-1, false);
+        };
+        Runnable queueOverflowMocker1 = () -> {
+            STATE.set(HealthManager.QUEUE_OVERFLOW);
+        };
+        Runnable queueOverflowMocker2 = () -> {
+            queueOverflowMocker1.run();
+            HealthManager.reportUsedTime(-1, true);
+        };
+        Assert rateIsNull = () -> {
+            assertNull(System.getProperty(ConfigConstants.CURRENT_RATE));
+        };
+        Assert rateNotNull = () -> {
+            assertNotNull(System.getProperty(ConfigConstants.CURRENT_RATE));
+        };
+        return Stream.of(
+                arguments(serviceExceptionMocker1, rateIsNull),
+                arguments(serviceExceptionMocker2, rateNotNull),
+                arguments(queueOverflowMocker1, rateIsNull),
+                arguments(queueOverflowMocker2, rateNotNull)
+        );
     }
 }
