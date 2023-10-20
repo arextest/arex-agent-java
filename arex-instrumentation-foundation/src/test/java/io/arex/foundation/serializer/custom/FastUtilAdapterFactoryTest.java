c36eef8a16458db67582d18ae3cf33f7db7a5cbb
dli39 <dli39@trip.com>
2023-10-20 11:21:16 +0800

    fix: resolve ClassCastException when supporting map type serialization under fastutil package

--- arex-instrumentation-foundation/src/test/java/io/arex/foundation/serializer/custom/FastUtilAdapterFactoryTest.java
+++ arex-instrumentation-foundation/src/test/java/io/arex/foundation/serializer/custom/FastUtilAdapterFactoryTest.java
@@ -2,39 +2,49 @@ package io.arex.foundation.serializer.custom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.google.common.reflect.TypeToken;
-import io.arex.agent.bootstrap.util.StringUtil;
 import io.arex.foundation.serializer.GsonSerializer;
-import io.arex.foundation.serializer.ProtoJsonSerializer;
-import io.arex.foundation.serializer.ProtoTest.HelloProtoRequestType;
 import it.unimi.dsi.fastutil.floats.FloatArrayList;
 import it.unimi.dsi.fastutil.floats.FloatList;
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 import it.unimi.dsi.fastutil.longs.LongLinkedOpenHashSet;
 import it.unimi.dsi.fastutil.longs.LongSet;
-import java.util.HashMap;
+import it.unimi.dsi.fastutil.objects.Object2DoubleArrayMap;
+import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import org.junit.jupiter.api.Assertions;
+import org.apache.commons.lang.math.DoubleRange;
 import org.junit.jupiter.api.Test;
 
 public class FastUtilAdapterFactoryTest {
+
     public static TestType getTestType() {
         final TestType testType = new TestType(1, "test");
+
         final IntOpenHashSet intOpenHashSet = new IntOpenHashSet();
         intOpenHashSet.add(2);
         testType.setIntSet(intOpenHashSet);
+
         final FloatArrayList floats = new FloatArrayList();
         floats.add(1.0f);
         testType.setFloats(floats);
+
         final LongLinkedOpenHashSet linkedOpenHashSet = new LongLinkedOpenHashSet();
         linkedOpenHashSet.add(3L);
         testType.setLongSet(linkedOpenHashSet);
+
         final HashSet<Integer> hashSet = new HashSet<>();
         hashSet.add(1);
         testType.setSet(new Set[]{hashSet});
+
+        final Object2DoubleMap<DoubleRange> object2DoubleMap = new Object2DoubleArrayMap<>(2);
+        final DoubleRange doubleRange1 = new DoubleRange(1, 3);
+        final DoubleRange doubleRange2 = new DoubleRange(6, 9);
+        object2DoubleMap.put(doubleRange1, 2);
+        object2DoubleMap.put(doubleRange2, 8);
+        testType.setObject2DoubleMap(object2DoubleMap);
+        testType.setDoubleRangeObject2DoubleMap(object2DoubleMap);
+
         return testType;
     }
 
@@ -47,12 +57,15 @@ public class FastUtilAdapterFactoryTest {
     }
 
     public static class TestType {
+
         private int id;
         private String name;
         private IntSet intSet;
         private FloatList floats;
         private LongSet longSet;
         private Set<Integer>[] set;
+        private Object2DoubleMap object2DoubleMap;
+        private Object2DoubleMap<DoubleRange> doubleRangeObject2DoubleMap;
 
         public TestType() {
         }
@@ -108,6 +121,23 @@ public class FastUtilAdapterFactoryTest {
 
         public void setSet(Set<Integer>[] set) {
             this.set = set;
+        }
+
+        public Object2DoubleMap getObject2DoubleMap() {
+            return object2DoubleMap;
+        }
+
+        public void setObject2DoubleMap(Object2DoubleMap object2DoubleMap) {
+            this.object2DoubleMap = object2DoubleMap;
+        }
+
+        public Object2DoubleMap<DoubleRange> getdoubleRangeObject2DoubleMap() {
+            return doubleRangeObject2DoubleMap;
+        }
+
+        public void setDoubleRangeObject2DoubleMap(
+            Object2DoubleMap<DoubleRange> doubleRangeObject2DoubleMap) {
+            this.doubleRangeObject2DoubleMap = doubleRangeObject2DoubleMap;
         }
     }
 }
