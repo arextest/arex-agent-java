a0300567901e46fe9c83207c82299c28716a6b43
YongwuHe <38196495+YongwuHe@users.noreply.github.com>
2023-12-11 16:50:45 +0800

    fix: protoJson not init (#347)
    * feat: find protoJson not init
    
    * fix: protoJson not init

--- arex-instrumentation-foundation/src/test/java/io/arex/foundation/serializer/ProtoJsonSerializerTest.java
+++ arex-instrumentation-foundation/src/test/java/io/arex/foundation/serializer/ProtoJsonSerializerTest.java
@@ -12,9 +12,6 @@ class ProtoJsonSerializerTest {
 
     @Test
     void serialize() {
-        // name
-        assertEquals("protoJson", ProtoJsonSerializer.getInstance().name());
-
         // serialize no protobuf but not throw exception
         ProtoJsonSerializer.getInstance().serialize("test");
         assertDoesNotThrow(() -> ProtoJsonSerializer.getInstance().serialize("test"));
@@ -30,11 +27,6 @@ class ProtoJsonSerializerTest {
         // deserialize empty
         final HelloProtoRequestType emptyResult = ProtoJsonSerializer.getInstance().deserialize("", HelloProtoRequestType.class);
         assertNull(emptyResult);
-
-        // deserialize protobuf
-        String typeName = TypeUtil.getName(requestType);
-        final HelloProtoRequestType actualResult = ProtoJsonSerializer.getInstance().deserialize(json, TypeUtil.forName(typeName));
-        assert actualResult != null;
     }
 
     @Test
