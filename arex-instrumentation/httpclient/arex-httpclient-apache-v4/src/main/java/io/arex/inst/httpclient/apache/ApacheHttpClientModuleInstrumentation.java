5bcb27623b856194624401c55fc4251d89f002d3
Mark Zhang <mr3.sole@gmail.com>
2024-02-22 14:09:39 +0800

    feat: support es java client (#407)

--- arex-instrumentation/httpclient/arex-httpclient-apache-v4/src/main/java/io/arex/inst/httpclient/apache/ApacheHttpClientModuleInstrumentation.java
+++ arex-instrumentation/httpclient/arex-httpclient-apache-v4/src/main/java/io/arex/inst/httpclient/apache/ApacheHttpClientModuleInstrumentation.java
@@ -4,6 +4,7 @@ import com.google.auto.service.AutoService;
 import io.arex.inst.extension.ModuleInstrumentation;
 import io.arex.inst.extension.TypeInstrumentation;
 
+import io.arex.inst.httpclient.apache.async.RequestProducerInstrumentation;
 import io.arex.inst.httpclient.apache.async.InternalHttpAsyncClientInstrumentation;
 import io.arex.inst.httpclient.apache.sync.InternalHttpClientInstrumentation;
 import java.util.List;
@@ -11,14 +12,15 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 @AutoService(ModuleInstrumentation.class)
-public class SyncClientModuleInstrumentation extends ModuleInstrumentation {
-    public SyncClientModuleInstrumentation() {
+public class ApacheHttpClientModuleInstrumentation extends ModuleInstrumentation {
+    public ApacheHttpClientModuleInstrumentation() {
         super("apache-httpclient-v4");
     }
 
     @Override
     public List<TypeInstrumentation> instrumentationTypes() {
         return asList(new InternalHttpClientInstrumentation(),
-            new InternalHttpAsyncClientInstrumentation());
+            new InternalHttpAsyncClientInstrumentation(),
+            new RequestProducerInstrumentation());
     }
 }
---
+++ arex-instrumentation/httpclient/arex-httpclient-apache-v4/src/test/java/io/arex/inst/httpclient/apache/async/RequestProducerInstrumentationTest.java
@@ -0,0 +1,59 @@
+package io.arex.inst.httpclient.apache.async;
+
+import static org.mockito.Mockito.mockStatic;
+
+import io.arex.inst.httpclient.apache.async.RequestProducerInstrumentation.ConstructorAdvice;
+import io.arex.inst.runtime.context.ContextManager;
+import java.lang.reflect.Constructor;
+import net.bytebuddy.description.method.MethodDescription.ForLoadedConstructor;
+import net.bytebuddy.description.type.TypeDescription;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.nio.protocol.BasicAsyncRequestProducer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+/**
+ * @since 2024/2/21
+ */
+class RequestProducerInstrumentationTest {
+
+    static RequestProducerInstrumentation target;
+
+    @BeforeAll
+    static void setUp() {
+        target = new RequestProducerInstrumentation();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        target = null;
+        Mockito.clearAllCaches();
+    }
+
+    @Test
+    void typeMatcher() {
+        Assertions.assertTrue(target.typeMatcher().matches(TypeDescription.ForLoadedType.of(BasicAsyncRequestProducer.class)));
+    }
+
+    @Test
+    void methodAdvices() throws NoSuchMethodException {
+        Constructor<BasicAsyncRequestProducer> create = BasicAsyncRequestProducer.class.getDeclaredConstructor(HttpHost.class, HttpRequest.class);
+        target.methodAdvices().get(0).getMethodMatcher().matches(new ForLoadedConstructor(create));
+    }
+
+    @Test
+    void onEnter() {
+        try (MockedStatic<ContextManager> contextManager = mockStatic(ContextManager.class)) {
+            contextManager.when(ContextManager::needRecordOrReplay).thenReturn(true);
+
+            HttpUriRequest request = Mockito.mock(HttpUriRequest.class);
+            ConstructorAdvice.onEnter(request);
+        }
+    }
+}
