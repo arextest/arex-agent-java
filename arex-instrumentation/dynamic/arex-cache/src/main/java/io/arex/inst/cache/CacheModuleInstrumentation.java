b8244093ce7614028a412f05a1035d2e670e8ad4
YongwuHe <38196495+YongwuHe@users.noreply.github.com>
2024-03-06 19:10:31 +0800

    feat: support guava cache (#377)

--- arex-instrumentation/dynamic/arex-cache/src/main/java/io/arex/inst/cache/CacheModuleInstrumentation.java
+++ arex-instrumentation/dynamic/arex-cache/src/main/java/io/arex/inst/cache/CacheModuleInstrumentation.java
@@ -1,14 +1,14 @@
 package io.arex.inst.cache;
 
 import com.google.auto.service.AutoService;
-
 import io.arex.inst.cache.arex.ArexMockInstrumentation;
+import io.arex.inst.cache.guava.GuavaCacheInstrumentation;
 import io.arex.inst.cache.spring.SpringCacheInstrumentation;
-import java.util.Arrays;
-import java.util.List;
-
 import io.arex.inst.extension.ModuleInstrumentation;
 import io.arex.inst.extension.TypeInstrumentation;
+
+import java.util.Arrays;
+import java.util.List;
 
 @AutoService(ModuleInstrumentation.class)
 public class CacheModuleInstrumentation extends ModuleInstrumentation {
@@ -18,6 +18,9 @@ public class CacheModuleInstrumentation extends ModuleInstrumentation {
 
     @Override
     public List<TypeInstrumentation> instrumentationTypes() {
-        return Arrays.asList(new SpringCacheInstrumentation(), new ArexMockInstrumentation());
+        return Arrays.asList(
+                new SpringCacheInstrumentation(),
+                new ArexMockInstrumentation(),
+                new GuavaCacheInstrumentation());
     }
 }
