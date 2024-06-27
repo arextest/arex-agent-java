649e41c16cffe03b86a1cd7a3802c3fe664c2523
yongwuhe <yongwuhe@trip.com>
2024-06-27 15:33:15 +0800

    feat: support comparable runnable

--- arex-agent-bootstrap/src/main/java/io/arex/agent/bootstrap/ctx/RunnableWrapper.java
+++ arex-agent-bootstrap/src/main/java/io/arex/agent/bootstrap/ctx/RunnableWrapper.java
@@ -2,10 +2,16 @@ package io.arex.agent.bootstrap.ctx;
 
 import io.arex.agent.bootstrap.TraceContextManager;
 
+import java.lang.reflect.Field;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class RunnableWrapper implements Runnable {
+    private static final ConcurrentHashMap<String, Optional<Field>> COMPARABLE_RUNNABLE_FIELD_MAP = new ConcurrentHashMap<>();
+    private static final AtomicBoolean EXECEPTION_LOGGED = new AtomicBoolean(false);
     private final Runnable runnable;
     private final TraceTransmitter traceTransmitter;
 
@@ -48,6 +54,50 @@ public class RunnableWrapper implements Runnable {
         if (runnable instanceof RunnableWrapper || runnable instanceof ForkJoinTask) {
             return runnable;
         }
+
+        if (runnable instanceof Comparable) {
+            wrapRunnableField(runnable);
+            return runnable;
+        }
         return new RunnableWrapper(runnable);
+    }
+
+    /**
+     * issue: https://github.com/arextest/arex-agent-java/issues/516
+     * executor with PriorityQueue
+     * ex: class PriorityRunnable extends Runnable implements Comparable<PriorityRunnable> {
+     *     private final Runnable run;
+     *     private final int priority;
+     * }
+     * can't wrap the PriorityRunnable because queue need runnable implement Comparable,
+     * so we need to wrap the original runnable field.
+     */
+    private static void wrapRunnableField(Runnable runnable) {
+        try {
+            Class<? extends Runnable> runnableClass = runnable.getClass();
+            Optional<Field> originalRunnableFieldOpt = COMPARABLE_RUNNABLE_FIELD_MAP.computeIfAbsent(runnableClass.getName(), k -> {
+                for (Field declaredField : runnableClass.getDeclaredFields()) {
+                    Class<?> declaredFieldType = declaredField.getType();
+                    if (declaredFieldType.isAssignableFrom(Runnable.class) && !declaredFieldType.isAssignableFrom(Comparable.class)) {
+                        declaredField.setAccessible(true);
+                        return Optional.of(declaredField);
+                    }
+                }
+                return Optional.empty();
+            });
+            if (originalRunnableFieldOpt.isPresent()) {
+                Field originalRunnableField = originalRunnableFieldOpt.get();
+                Runnable originalRunnable = (Runnable) originalRunnableField.get(runnable);
+                if (originalRunnable instanceof RunnableWrapper) {
+                    return;
+                }
+                RunnableWrapper runnableWrapper = new RunnableWrapper(originalRunnable);
+                originalRunnableField.set(runnable, runnableWrapper);
+            }
+        } catch (Exception e) {
+            if (EXECEPTION_LOGGED.compareAndSet(false, true)) {
+                System.err.printf("wrap original runnable %s failed.%n", runnable.getClass().getName());
+            }
+        }
     }
 }
