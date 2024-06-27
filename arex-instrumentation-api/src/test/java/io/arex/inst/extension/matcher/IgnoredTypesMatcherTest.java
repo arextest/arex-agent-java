0c69061810641c61f58af0a4b7ef17de20e16fb5
LHHDZ <shichanglin5@qq.com>
2024-06-27 11:26:01 +0800

    fix tests IgnoredTypesMatcherTest

--- arex-instrumentation-api/src/test/java/io/arex/inst/extension/matcher/IgnoredTypesMatcherTest.java
+++ arex-instrumentation-api/src/test/java/io/arex/inst/extension/matcher/IgnoredTypesMatcherTest.java
@@ -3,13 +3,15 @@ package io.arex.inst.extension.matcher;
 import net.bytebuddy.description.type.TypeDescription;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IgnoredTypesMatcherTest {
 
     @Test
     void matches() {
-        IgnoredTypesMatcher matcher = new IgnoredTypesMatcher(null);
+        IgnoredTypesMatcher matcher = new IgnoredTypesMatcher(Collections.emptyList());
         assertTrue(matcher.matches(new TypeDescription.ForLoadedType(IgnoredTypesMatcher.class)));
         assertFalse(matcher.matches(new TypeDescription.ForLoadedType(String.class)));
     }
