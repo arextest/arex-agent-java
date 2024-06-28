14687f74e71dc0f127dd3bf459223812b2fbf965
YongwuHe <38196495+YongwuHe@users.noreply.github.com>
2024-06-28 17:37:57 +0800

    feat: support comparable runnable (#517)

--- arex-agent-bootstrap/src/test/java/io/arex/agent/bootstrap/ctx/RunnableWrapperTest.java
+++ arex-agent-bootstrap/src/test/java/io/arex/agent/bootstrap/ctx/RunnableWrapperTest.java
@@ -3,6 +3,9 @@ package io.arex.agent.bootstrap.ctx;
 import io.arex.agent.bootstrap.TraceContextManager;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RunnableFuture;
 
@@ -11,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RunnableWrapperTest {
 
     @Test
-    void get() {
+    void get() throws Exception {
         assertNull(RunnableWrapper.get(null));
         TraceContextManager.set("mock");
         Runnable objectRunnable = RunnableWrapper.get(new RunnableTest<>());
@@ -20,7 +23,29 @@ class RunnableWrapperTest {
         assertDoesNotThrow(emptyRunnable::run);
         assertNotNull(emptyRunnable.toString());
         assertTrue(emptyRunnable.hashCode() > 0);
-        assertFalse(emptyRunnable.equals(objectRunnable));
+        assertNotEquals(emptyRunnable, objectRunnable);
+
+        NoRunnableFieldPriorityRunnable noRunnableFieldComparableRunnable = new NoRunnableFieldPriorityRunnable(1);
+        RunnableWrapper.get(noRunnableFieldComparableRunnable);
+        Field comparableRunnableFieldMap = RunnableWrapper.class.getDeclaredField("COMPARABLE_RUNNABLE_FIELD_MAP");
+        comparableRunnableFieldMap.setAccessible(true);
+        Map<String, Optional<Field>> map = (Map<String, Optional<Field>>) comparableRunnableFieldMap.get(null);
+        assertNotNull(map);
+        assertFalse(map.get(noRunnableFieldComparableRunnable.getClass().getName()).isPresent());
+        Runnable originalRunnable = () -> {};
+        PriorityRunnable priorityRunnable = new PriorityRunnable(originalRunnable, 1);
+        Runnable runnable2 = RunnableWrapper.get(priorityRunnable);
+        assertSame(priorityRunnable, runnable2);
+        assertEquals("run", map.get(priorityRunnable.getClass().getName()).get().getName());
+        assertInstanceOf(RunnableWrapper.class, priorityRunnable.getRun());
+        // runnable field already wrapped
+        RunnableWrapper.get(priorityRunnable);
+        Runnable run = priorityRunnable.getRun();
+        assertInstanceOf(RunnableWrapper.class, run);
+        Field runnable = RunnableWrapper.class.getDeclaredField("runnable");
+        runnable.setAccessible(true);
+        assertFalse(runnable.get(run) instanceof RunnableWrapper);
+
         TraceContextManager.remove();
     }
 
@@ -29,5 +54,55 @@ class RunnableWrapperTest {
         public final void setRawResult(T v) {}
         public final boolean exec() { return true; }
         public final void run() {}
+    }
+
+    public static class NoRunnableFieldPriorityRunnable implements Runnable, Comparable<NoRunnableFieldPriorityRunnable> {
+        private final int priority;
+
+        public NoRunnableFieldPriorityRunnable(int priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public int compareTo(NoRunnableFieldPriorityRunnable other) {
+            int res = 0;
+            if (this.priority != other.priority) {
+                res = this.priority > other.priority ? -1 : 1;
+            }
+            return res;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("PriorityRunnable.run");
+        }
+    }
+
+    static class PriorityRunnable implements Runnable, Comparable<PriorityRunnable> {
+        private final Runnable run;
+        private final int priority;
+
+        public PriorityRunnable(Runnable run, int priority) {
+            this.run = run;
+            this.priority = priority;
+        }
+
+        @Override
+        public int compareTo(PriorityRunnable other) {
+            int res = 0;
+            if (this.priority != other.priority) {
+                res = this.priority > other.priority ? -1 : 1;
+            }
+            return res;
+        }
+
+        @Override
+        public void run() {
+            this.run.run();
+        }
+
+        public Runnable getRun() {
+            return run;
+        }
     }
 }
