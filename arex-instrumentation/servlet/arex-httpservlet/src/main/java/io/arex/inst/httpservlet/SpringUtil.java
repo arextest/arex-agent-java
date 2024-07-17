41e91b4626c5bc9c52dab084cc5ec5e078dc4a17
Lucas <vipmyx8651@163.com>
2024-07-17 15:31:41 +0800

    feat: compatible spring older versions and optimize reflect (#540)
    * feat: compatible spring older versions and optimize reflect
    
    * fix: sonar

--- arex-instrumentation/servlet/arex-httpservlet/src/main/java/io/arex/inst/httpservlet/SpringUtil.java
+++ arex-instrumentation/servlet/arex-httpservlet/src/main/java/io/arex/inst/httpservlet/SpringUtil.java
@@ -2,14 +2,16 @@ package io.arex.inst.httpservlet;
 
 import io.arex.agent.bootstrap.util.CollectionUtil;
 import io.arex.agent.bootstrap.util.ReflectUtil;
+import io.arex.inst.runtime.util.fastreflect.MethodHolder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import org.springframework.web.util.ServletRequestPathUtils;
-import org.springframework.web.util.pattern.PathPattern;
 
 import java.lang.reflect.Method;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -18,21 +20,39 @@ import java.util.Set;
  */
 public class SpringUtil {
 
-    private static Method getMatchMethod = null;
-
     private static Method parseAndCacheMethod = null;
 
     private static ApplicationContext springApplicationContext = null;
 
     private static Set<RequestMappingInfo> springRequestMappings = null;
 
+    private static final Class<?> SERVLET_REQUEST_PATH_CLASS =
+            ReflectUtil.getClass("org.springframework.web.util.ServletRequestPathUtils");
+
+    private static final Method GET_PATTERN_VALUES_METHOD =
+            ReflectUtil.getMethodWithoutClassType(RequestMappingInfo.class.getName(), "getPatternValues");
+
+    private static final String SERVLET_REQUEST_JAVAX = "javax.servlet.http.HttpServletRequest";
+
+    private static final String SERVLET_REQUEST_JAKARTA = "jakarta.servlet.http.HttpServletRequest";
+
+    private static final String SERVLET_CONTEXT_JAVAX = "javax.servlet.ServletContext";
+
+    private static final String SERVLET_CONTEXT_JAKARTA = "jakarta.servlet.ServletContext";
+
+    private static MethodHolder<RequestMappingInfo> getMatchMethodHolder = null;
+
     public static ApplicationContext getApplicationContext(Object servletContext) {
         if (springApplicationContext != null) {
             return springApplicationContext;
         }
-        Method findWebApplicationContextMethod = ReflectUtil.getMethodWithoutClassType(WebApplicationContextUtils.class.getName(),
-                "findWebApplicationContext", servletContext.getClass().getInterfaces()[0].getName());
-        springApplicationContext = (ApplicationContext) ReflectUtil.invoke(findWebApplicationContextMethod, null, servletContext);
+        Method getWebApplicationContextMethod = ReflectUtil.getMethodWithoutClassType(WebApplicationContextUtils.class.getName(),
+                "getWebApplicationContext", SERVLET_CONTEXT_JAVAX);
+        if (getWebApplicationContextMethod == null) {
+            getWebApplicationContextMethod = ReflectUtil.getMethodWithoutClassType(WebApplicationContextUtils.class.getName(),
+                    "getWebApplicationContext", SERVLET_CONTEXT_JAKARTA);
+        }
+        springApplicationContext = (ApplicationContext) ReflectUtil.invoke(getWebApplicationContextMethod, null, servletContext);
         return springApplicationContext;
     }
 
@@ -48,6 +68,12 @@ public class SpringUtil {
         return springRequestMappings;
     }
 
+    /**
+     * By use reflection method to mask the differences between servlet versions, it achieves common use
+     * @param httpServletRequest javax.servlet.http.HttpServletRequest or jakarta.servlet.http.HttpServletRequest
+     * @param servletContext javax.servlet.ServletContext or jakarta.servlet.ServletContext
+     * @return
+     */
     public static String getPatternFromRequestMapping(Object httpServletRequest, Object servletContext) {
         Set<RequestMappingInfo> requestMappingInfos = getAllRequestMappingInfo(servletContext);
         if (CollectionUtil.isEmpty(requestMappingInfos)) {
@@ -56,20 +82,32 @@ public class SpringUtil {
 
         setPathAttribute(httpServletRequest);
 
-        if (getMatchMethod == null) {
-            getMatchMethod = ReflectUtil.getMethodWithoutClassType(RequestMappingInfo.class.getName(),
-                    "getMatchingCondition", httpServletRequest.getClass().getInterfaces()[0].getName());
+        if (getMatchMethodHolder == null) {
+            Method getMatchMethod = ReflectUtil.getMethodWithoutClassType(RequestMappingInfo.class.getName(),
+                    "getMatchingCondition", SERVLET_REQUEST_JAVAX);
+            if (getMatchMethod == null) {
+                getMatchMethod = ReflectUtil.getMethodWithoutClassType(RequestMappingInfo.class.getName(),
+                        "getMatchingCondition", SERVLET_REQUEST_JAKARTA);
+            }
+            getMatchMethodHolder = MethodHolder.build(getMatchMethod);
         }
 
-        Set<PathPattern> patterns = null;
+        Set<String> patterns = new HashSet<>();
+        PatternsRequestCondition patternsCondition;
         for (RequestMappingInfo mappingInfo : requestMappingInfos) {
             /*
-             * execute RequestMappingInfo.getMatchingCondition to get matched pattern
+             * fast reflect RequestMappingInfo.getMatchingCondition to get matched pattern
              * eg: /user/lucas/22 -> /user/{name}/{age}
              */
-            RequestMappingInfo matchMapping = (RequestMappingInfo) ReflectUtil.invoke(getMatchMethod, mappingInfo, httpServletRequest);
-            if (matchMapping != null && matchMapping.getPathPatternsCondition() != null) {
-                patterns = matchMapping.getPathPatternsCondition().getPatterns();
+            RequestMappingInfo matchMapping = getMatchMethodHolder.invoke(mappingInfo, httpServletRequest);
+            if (matchMapping != null) {
+                patternsCondition = matchMapping.getPatternsCondition();
+                // getPatternValues is available in Spring 5.3.0+
+                if (GET_PATTERN_VALUES_METHOD != null) {
+                    patterns.addAll(matchMapping.getPatternValues());
+                } else if (patternsCondition != null) {
+                    patterns.addAll(patternsCondition.getPatterns());
+                }
                 break;
             }
         }
@@ -77,13 +115,21 @@ public class SpringUtil {
         if (CollectionUtil.isEmpty(patterns)) {
             return null;
         }
-        return patterns.iterator().next().getPatternString();
+        return patterns.iterator().next();
     }
 
     private static void setPathAttribute(Object httpServletRequest) {
+        // ServletRequestPathUtils.class is available in Spring 5.3.0+
+        if (SERVLET_REQUEST_PATH_CLASS == null) {
+            return;
+        }
         if (parseAndCacheMethod == null) {
             parseAndCacheMethod = ReflectUtil.getMethodWithoutClassType(ServletRequestPathUtils.class.getName(),
-                    "parseAndCache", httpServletRequest.getClass().getInterfaces()[0].getName());
+                    "parseAndCache", SERVLET_REQUEST_JAVAX);
+            if (parseAndCacheMethod == null) {
+                parseAndCacheMethod = ReflectUtil.getMethodWithoutClassType(ServletRequestPathUtils.class.getName(),
+                        "parseAndCache", SERVLET_REQUEST_JAKARTA);
+            }
         }
         // execute ServletRequestPathUtils.parseAndCache, because getMatchingCondition need path attribute from request
         ReflectUtil.invoke(parseAndCacheMethod, null, httpServletRequest);
