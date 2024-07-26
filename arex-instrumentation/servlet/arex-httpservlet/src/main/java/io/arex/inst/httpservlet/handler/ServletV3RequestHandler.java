b7cf3fbd86f984daff33d1acfe553aba3a0aa050
lucas-myx <vipmyx8651@163.com>
2024-07-26 21:57:00 +0800

    feat: remove trace id

--- arex-instrumentation/servlet/arex-httpservlet/src/main/java/io/arex/inst/httpservlet/handler/ServletV3RequestHandler.java
+++ arex-instrumentation/servlet/arex-httpservlet/src/main/java/io/arex/inst/httpservlet/handler/ServletV3RequestHandler.java
@@ -1,14 +1,11 @@
 package io.arex.inst.httpservlet.handler;
 
 import com.google.auto.service.AutoService;
-import io.arex.agent.bootstrap.util.StringUtil;
-import io.arex.inst.runtime.context.ContextManager;
 import io.arex.inst.runtime.model.ArexConstants;
 import io.arex.inst.runtime.request.RequestHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.UUID;
 
 
 @AutoService(RequestHandler.class)
@@ -25,12 +22,7 @@ public class ServletV3RequestHandler implements RequestHandler<HttpServletReques
 
     @Override
     public void handleAfterCreateContext(HttpServletRequest request) {
-        String traceId = request.getHeader("arex-trace-id");
-        if (StringUtil.isEmpty(traceId)) {
-            traceId = UUID.randomUUID().toString();
-        }
-        ContextManager.setAttachment("arex-trace-id", traceId);
-        ContextManager.setAttachment("arex-client-app-id", request.getHeader("arex-client-app-id"));
+        // no need implement
     }
 
     @Override
