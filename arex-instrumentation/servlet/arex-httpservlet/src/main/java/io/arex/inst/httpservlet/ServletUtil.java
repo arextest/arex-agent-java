d74beeb0271d93fec77fc87b1084dfc7a51f6b10
qzmo <qzmo@trip.com>
2024-12-12 15:22:58 +0800

    fix: uri append

--- arex-instrumentation/servlet/arex-httpservlet/src/main/java/io/arex/inst/httpservlet/ServletUtil.java
+++ arex-instrumentation/servlet/arex-httpservlet/src/main/java/io/arex/inst/httpservlet/ServletUtil.java
@@ -20,22 +20,7 @@ public class ServletUtil {
     }
 
     public static String appendUri(String uri, String name, String value) {
-        try {
-            URI oldUri = URI.create(uri);
-            StringBuilder builder = new StringBuilder();
-            String newQuery = oldUri.getQuery();
-            if (oldUri.getQuery() == null) {
-                builder.append(name).append("=").append(value);
-            } else {
-                builder.append(newQuery).append("&").append(name).append("=").append(value);
-            }
-
-            URI newUri = new URI(oldUri.getScheme(), oldUri.getAuthority(),
-                oldUri.getPath(), builder.toString(), oldUri.getFragment());
-            return newUri.toString();
-        } catch (URISyntaxException e) {
-            return uri;
-        }
+        return UriComponentsBuilder.fromUriString(uri).queryParam(name, value).build().toString();
     }
 
     public static String getRequestPath(String uri) {
